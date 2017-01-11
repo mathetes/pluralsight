68629f47af47af7c6c3f8ff05e786c5a2e8586d1
mathetes <nsk.roman@gmail.com>
2017-01-11 03:23:27 -0800

    Add page courses

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,10 +1,12 @@
-import React, { ProtoTypes } from 'react';
+import React, {ProtoTypes} from 'react';
 import { Link, IndexLink } from 'react-router';
 
 const Header = () => {
 		return (
 			<nav>
 				<IndexLink to="/" activeClassName="active">Home</IndexLink>
+				{" | "}
+				<Link to="/courses" activeClassName="active">Courses</Link>
 				{" | "}
 				<Link to="/about" activeClassName="active">About</Link>
 			</nav>
