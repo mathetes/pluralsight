84227374167fa25e8aa9f51e6d535679d805e51b
mathetes <nsk.roman@gmail.com>
2017-01-12 04:54:44 -0800

    Redux input

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -1,10 +1,43 @@
 import React, { PropTypes } from 'react';
 
 class CoursesPage extends React.Component {
+		constructor(props, context) {
+		super(props, context);
+
+		this.state = {
+			course: { title: "" }
+		};		
+
+		this.onTitleChange = this.onTitleChange.bind(this);
+		this.onClickSave = this.onClickSave.bind(this);
+	}
+
+	onTitleChange(event) {
+		const course = this.state.course;
+		course.title = event.target.value;
+		this.setState( {course: course} );
+	}
+
+	onClickSave() {
+		alert('Saving ${this.state.course.title}');
+	}
+
 	render() {
 		return (
 			<div>
-				<h1>Courses</h1>
+				<div className="row">
+					<h1>Courses</h1>
+					<h2>Add Course</h2>
+					<input
+						type="text"
+						onChange={this.onTitleChange}
+						value={this.state.course.title} />
+					<input
+						className="btn btn-success"
+						type="submit"
+						value="Save"
+						onClick={this.onClickSave} />
+				</div>
 				<div className="col-lg-4">
 					<img src="http://lorempixel.com/240/120/sports/" />
 					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
