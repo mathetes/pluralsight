164381f2b7ee8b83593bdcad3cc0eeb88fce1b82
mathetes <nsk.roman@gmail.com>
2017-04-14 21:05:26 +0700

    step95 done

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -3,7 +3,7 @@ import courseApi from '../api/mockCourseApi';
 
 
 export function loadCoursesSuccess(courses) {
-    return { type: types.LOAD_COURSES_SUCCESS, courses }
+    return { type: types.LOAD_COURSES_SUCCESS, courses };
 }
 
 export function loadCourses() {
