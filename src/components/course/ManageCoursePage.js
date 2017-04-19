404dc29cdd990c1404ebef5c9426b8a3d468418f
mathetes <nsk.roman@gmail.com>
2017-04-19 15:06:05 +0700

    step109 done

--- src/components/course/ManageCoursePage.js
+++ src/components/course/ManageCoursePage.js
@@ -26,6 +26,7 @@ class ManageCoursePage extends React.Component {
     saveCourse(event) {
         event.preventDefault();
         this.props.actions.saveCourse(this.state.course);
+        this.context.router.push('/courses');
     }
 
 
@@ -46,6 +47,10 @@ ManageCoursePage.propTypes = {
     course: PropTypes.object.isRequired,
     authors: PropTypes.array.isRequired,
     actions: PropTypes.object.isRequired
+};
+
+ManageCoursePage.contextTypes = {
+    router: PropTypes.object.isRequired
 };
 
 function mapStateToProps(state, ownProps) {
